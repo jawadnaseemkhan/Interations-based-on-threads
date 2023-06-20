b0856295cd98fdf7c517bfbc9d8534d2b38354f7
Jawad Naseem Khan <jawadnaseemkhan@gmail.com>
2023-06-20 04:45:26 +0200

    Added the comments on codes

--- MCP2/Test1.c
+++ MCP2/Test1.c
@@ -5,10 +5,10 @@ void main(void)
 {
 	int i;
 
-	omp_set_num_threads(2);
+	omp_set_num_threads(2); //Enter the number of threads in brackets that are required 
 
-#pragma omp parallel
-#pragma omp for
+#pragma omp parallel		// Used for parallel threading
+#pragma omp for				// Used as per the iterations
 	for(i = 0; i<10; i++)
 	{
 		printf("This iteration is %d and is using thread %d\n", i, omp_get_thread_num());
